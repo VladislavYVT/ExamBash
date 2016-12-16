61a3d1fec9b0bb10cf943cbe83251d52245ccbd4
Vlod <yavtukhovskiyv@gmail.com>
2016-12-16 15:04:11 +0200

    car

---
+++ car.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+struct Car {
+	int Number;
+};
+
+int main(int argc, char** argv) {
+	return 0;
+}
