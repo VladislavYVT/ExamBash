9c1121a33e2480dc38179ab5ac0916aae5e28e5f
Vlod <yavtukhovskiyv@gmail.com>
2016-12-16 14:51:07 +0200

    Human

---
+++ Human.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+struct Human {
+	int age;
+};
+
+
+int main(int argc, char** argv) {
+	return 0;
+}
